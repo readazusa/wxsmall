84179f0d322ee9e347cfcf14fee1d2d2a312b97a
smc <smc@oceansoft.com.cn>
2017-02-28 17:20:28 +0800

    评论的编写

--- src/main/java/club/lovety/wx/application/comment/service/impl/CommentServiceImpl.java
+++ src/main/java/club/lovety/wx/application/comment/service/impl/CommentServiceImpl.java
@@ -5,10 +5,13 @@ import club.lovety.wx.application.comment.entity.CommentInfo;
 import club.lovety.wx.application.comment.service.ICommentService;
 import club.lovety.wx.base.entity.BasePageInfo;
 import club.lovety.wx.base.entity.BaseSearchInfo;
+import club.lovety.wx.common.IdWorker;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created by 念梓  on 2017/2/28.
@@ -24,6 +27,9 @@ public class CommentServiceImpl implements ICommentService {
 
     @Override
     public int save(CommentInfo commentInfo) {
+        commentInfo.setUid(IdWorker.instance.getId());
+        commentInfo.setCreateTime(new Date());
+        commentInfo.setUpdateTime(new Date());
         return commentDao.save(commentInfo);
     }
 
@@ -39,6 +45,17 @@ public class CommentServiceImpl implements ICommentService {
 
     @Override
     public BasePageInfo<CommentInfo> queryBasePageInfo(HttpServletRequest request) {
-        return null;
+        BaseSearchInfo<CommentInfo> baseSearchInfo = new BaseSearchInfo<>();
+        String pageIndex = request.getParameter("pageIndex");
+        baseSearchInfo.setPageIndex((Integer.parseInt(pageIndex)-1)*baseSearchInfo.getPageSize());
+        CommentInfo commentInfo = new CommentInfo();
+        commentInfo.setRelationUid(Long.parseLong(request.getParameter("relationId")));
+        baseSearchInfo.setT(commentInfo);
+        BasePageInfo<CommentInfo> basePageInfo = new BasePageInfo<>();
+        List<CommentInfo> commentInfoList = commentDao.queryPage(baseSearchInfo);
+        int totalCount = commentDao.queryTotalCount(commentInfo);
+        basePageInfo.setData(commentInfoList);
+        basePageInfo.setTotalCount(totalCount);
+        return basePageInfo;
     }
 }
